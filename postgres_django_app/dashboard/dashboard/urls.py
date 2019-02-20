804b44e80aede9f83e7aedf28f9a6ed17e82ff14
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-02-21 02:24:46 +0530

    Added static file

--- postgres_django_app/dashboard/dashboard/urls.py
+++ postgres_django_app/dashboard/dashboard/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 from django.urls import path
 
+
 urlpatterns = [
-                  path('', lambda request: HttpResponse("Hello"), name="index"),
+                  path('', TemplateView.as_view(template_name='sheets/index.html'), name="index"),
                   path('admin/', admin.site.urls),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
