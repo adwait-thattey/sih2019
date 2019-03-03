ce31b6fb9fb609d08498b47843063362724e9e3b
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-03-03 07:04:13 +0530

    connected to postgre

--- postgres_django_app/dashboard/sheets/static/sheets/js/custom-chart.js
+++ postgres_django_app/dashboard/sheets/static/sheets/js/custom-chart.js
@@ -1,25 +1,16 @@
 // // Function to get the start to End Year
+
 // // Returns an array like ['2012-13','2013-14'..'2017-18']
-// function getYears(graphData) {
-//     // need graph data to find the oldest year
-//     var oldestYear = graphData[0].start_year;
-//     var yearCount = 0;
-//
-//     var yearList = [];
-//
-//     for (var yearData of graphData) {
-//         if (oldestYear > yearData.start_year) oldestYear = yearData.start_year;
-//         for (var dateLength in yearData.values) {
-//             if (yearData.values[dateLength].length > yearCount) yearCount = yearData.values[dateLength].length;
-//         }
-//     }
-//
-//     // Max yearCount received
-//     for (var i=0, j=oldestYear; i<yearCount; i++,j++) {
-//         yearList.push(j);
-//     }
-//     return yearList;
-// }
+function getYears(data, property) {
+
+    var yearList = [];
+
+    var startingYear = data[property];
+
+
+
+    return yearList;
+}
 //
 //
 // function getSeriesData(graphDataValues) {
@@ -39,35 +30,40 @@
 // }
 
 
-var dummyJson = {
 
-    activity_id:4,
-    activity_name: 'Agriculture',
-    GVA: {
-        current: [4, 8, 12, 93, 7],
-        constant: [89, 74, 31, 2, 9]
-    },
-    NVA: {
-        current: [4, 8, 12, 93, 7],
-        constant: [89, 74, 31, 2, 9]
-    },
-    Indices: {
-        price: [8,9,32,11,07],
-        quantum: [95,77,30,3,17]
+//
+// buildTimeSeriesChart('gva-container-timeSeries1');
+// buildPieChart('gva-container-pie1');
+// buildDualChart('gva-container-db1');
+//
+
+function buildTimeSeriesChart(htmlId, data, type) {
+
+    var twoChildAttr = [];
+    var text;
+    var types = [];
+
+    if (type === 'GVA') {
+        twoChildAttr = [data[type].current, data[type].constant];
+        text = 'Current and constant GVA Values';
+        types = ['current', 'constant'];
     }
-};
+    if (type === 'NVA') {
+        twoChildAttr = [data[type].current, data[type].constant];
+        text = 'Current and constant GVA Values';
+        types = ['current', 'constant'];
+    }
+    if (type === 'indices') {
+        twoChildAttr = [data[type].price, data[type].quantum];
+        text = 'Price and Quantum GVA Values';
+        types = ['price', 'quantum'];
+    }
 
 
-buildTimeSeriesChart('gva-container-timeSeries1');
-buildPieChart('gva-container-pie1');
-buildDualChart('gva-container-db1');
-
-
-function buildTimeSeriesChart(htmlId) {
     Highcharts.chart(htmlId, {
 
         title: {
-            text: 'Current and constant GVA Values'
+            text: text
         },
 
         subtitle: {
@@ -90,16 +86,16 @@ function buildTimeSeriesChart(htmlId) {
                 label: {
                     connectorAllowed: false
                 },
-                pointStart: 2010
+                pointStart: data[type].start_year
             }
         },
 
         series: [{
-            name: 'Current',
-            data: [10000, 52503, 57177, 69658, 97031, 119931]
+            name: types[0],
+            data: twoChildAttr[0]
         }, {
-            name: 'Constant',
-            data: [24916, 24064, 29742, 29851, 32490, 30282]
+            name: types[1],
+            data: twoChildAttr[1]
         }],
 
         responsive: {
@@ -122,27 +118,8 @@ function buildTimeSeriesChart(htmlId) {
 
 
 
-function buildPieChart(htmlId) {
+function buildPieChart(htmlId, data) {
 
-    // graphDataValues, htmlId
-    // var seriesData = getSeriesData(graphDataValues);
-
-    // var renamedObject = [];
-    // for (var object of seriesData) {
-    //     var sumOfData = 0;
-    //
-    //     for (var dataValues of object.data) {
-    //         sumOfData += dataValues
-    //     }
-    //
-    //     var newObject = {
-    //         name: object.name,
-    //         y: sumOfData
-    //     };
-    //     renamedObject.push(newObject);
-    // }
-    //
-    // console.log(renamedObject);
 
     Highcharts.chart(htmlId, {
         chart: {
@@ -201,9 +178,17 @@ function buildPieChart(htmlId) {
 
 function buildLineChart(htmlId) {
 
-    // parameter : graphDataValues, htmlId
+    var twoChildAttr = [];
 
-    // var seriesData = getSeriesData(graphDataValues);
+    if (type === 'GVA') {
+        twoChildAttr = [data[type].current, data[type].constant];
+    }
+    if (type === 'NVA') {
+        twoChildAttr = [data[type].current, data[type].constant];
+    }
+    if (type === 'indices') {
+        twoChildAttr = [data[type].price, data[type].quantum];
+    }
 
     Highcharts.chart(htmlId, {
 
@@ -237,19 +222,10 @@ function buildLineChart(htmlId) {
 
         series: [{
             name: 'Installation',
-            data: [45000, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+            data: twoChildAttr[0]
         }, {
             name: 'Manufacturing',
-            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-        }, {
-            name: 'Sales & Distribution',
-            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-        }, {
-            name: 'Project Development',
-            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-        }, {
-            name: 'Other',
-            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+            data: twoChildAttr[1]
         }],
 
         responsive: {
@@ -272,9 +248,20 @@ function buildLineChart(htmlId) {
 };
 
 
-function buildDualChart(htmlId) {
+function buildDualChart(htmlId, data, type) {
 
-    // var seriesData = getSeriesData(graphDataValues);
+    var twoChildAttr = [];
+
+    if (type === 'GVA') {
+        twoChildAttr = [data[type].current, data[type].constant];
+    }
+    if (type === 'NVA') {
+        twoChildAttr = [data[type].current, data[type].constant];
+    }
+    if (type === 'indices') {
+        twoChildAttr = [data[type].price, data[type].quantum];
+    }
+
 
    
     Highcharts.chart(htmlId, {
@@ -328,64 +315,16 @@ function buildDualChart(htmlId) {
 
         series: [{
             name: 'Current',
-            data: [5611895, 1818173, 1713364, 1817178, 178172],
+            data: twoChildAttr[0],
             stack: 'male'
         }, {
             name: 'Constant',
-            data: [3451142, 636514, 4315514, 4514774, 3189618],
+            data: twoChildAttr[1],
             stack: 'female'
         }]
     });
 }
 
-
-Highcharts.chart('gva-container-pie2', {
-    chart: {
-        type: 'pie',
-        options3d: {
-            enabled: true,
-            alpha: 45,
-            beta: 0
-        }
-    },
-    title: {
-        text: 'Current and constant GVA Values'
-    },
-    subtitle: {
-            text: 'Source: mospi.gov.in'
-        },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            depth: 35,
-            dataLabels: {
-                enabled: true,
-                format: '{point.name}'
-            }
-        }
-    },
-    series: [{
-        type: 'pie',
-        name: 'Browser share',
-        data: [
-            ['2011', 8.8],
-            ['2012', 39],
-            {
-                name: '2013',
-                y: 12.8,
-                sliced: true,
-                selected: true
-            },
-            ['2014', 22.5],
-            ['2015', 6.2],
-            ['2016', 10.7]
-        ]
-    }]
-});
 
 
 
